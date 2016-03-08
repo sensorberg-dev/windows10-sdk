30c8cfd91548450c2ffbd462789e9afe9d5a88d8
kay <kay.czarnotta@maxdome.de>
2016-03-08 09:31:06 +0100

    add url to inapp actions

--- SensorbergSDKTests/LayoutManagerTest.cs
+++ SensorbergSDKTests/LayoutManagerTest.cs
@@ -27,7 +27,7 @@ namespace SensorbergSDKTests
         public async Task TestValidLayout()
         {
             LayoutManager manager = LayoutManager.Instance;
-            Assert.IsTrue(await manager.VerifyLayoutAsync(true), "Verification failed" );
+            Assert.IsTrue(await manager.VerifyLayoutAsync(true), "Verification failed");
             Layout layout = manager.Layout;
             Assert.IsNotNull(layout, "No Layout avialable");
             Assert.AreEqual(4, layout.AccountBeaconId1s.Count, "Number of proximity beacons not matching");
@@ -37,12 +37,23 @@ namespace SensorbergSDKTests
             Assert.IsTrue(layout.AccountBeaconId1s.Contains("7367672374000000ffff0000ffff0007"), "Beacon 4 not found");
             Assert.AreEqual(8, layout.ResolvedActions.Count, "not 8 actions");
 
-            ResolvedAction a = layout.ResolvedActions.FirstOrDefault(t => t.BeaconAction.Uuid == "3f30be2605524f82a9bf0ccb4a81618f");
-            Assert.AreEqual(1, (int)a.EventTypeDetectedByDevice, "Wrong trigger type");
-            Assert.AreEqual(1, a.BeaconPids.Count,"Beacon count wrong");
+            ResolvedAction a = layout.ResolvedActions.FirstOrDefault(t => t.BeaconAction.Uuid == "9ded63644e424d758b0218f7c70f2473");
+            Assert.AreEqual(3, (int) a.EventTypeDetectedByDevice, "Wrong trigger type");
+            Assert.AreEqual(1, a.BeaconPids.Count, "Beacon count wrong");
             Assert.IsTrue(a.BeaconPids.ContainsKey("7367672374000000ffff0000ffff00043917830929"), "No Beacon found!");
 
             Assert.AreEqual(43200, a.SupressionTime, "Wrong supression time!");
+
+            Assert.AreEqual(string.Empty, a.BeaconAction.Subject, "Different action subject");
+            Assert.AreEqual(string.Empty, a.BeaconAction.Body, "Different action body");
+            Assert.AreEqual("http://www.visitfinland.com/", a.BeaconAction.Url, "wrong url is set");
+            Assert.IsNull(a.BeaconAction.Payload, "Payload is not null");
+
+            Assert.AreEqual(1, a.Timeframes.Count, "More timeframes are set");
+            Assert.AreEqual(new DateTime(2015, 04, 16, 12, 46, 19, 627), a.Timeframes[0].Start.Value.DateTime, "Different timesetting");
+
+            Assert.AreEqual(3, (int)a.BeaconAction.Type, "Different type");
+            Assert.IsFalse(a.SendOnlyOnce, "Send only once is set");
         }
     }
 }
