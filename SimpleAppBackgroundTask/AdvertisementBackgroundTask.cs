f18f50769ec7044c5115b061d72be12544d355ee
kay <eagleeye@byte-welt.net>
2016-05-12 18:46:45 +0200

    add BackgroundTask Project to All solution

---
+++ SimpleAppBackgroundTask/AdvertisementBackgroundTask.cs
@@ -0,0 +1,28 @@
+﻿// Created by Kay Czarnotta on 12.05.2016
+// 
+// Copyright (c) 2016,  Sensorberg
+// 
+// All rights reserved.
+
+using System.Diagnostics;
+using Windows.ApplicationModel.Background;
+using SensorbergSDK.SensorbergSDKBackground;
+
+namespace SimpleAppBackgroundTask
+{
+    public sealed class AdvertisementBackgroundTask:IBackgroundTask
+    {
+        private AdvertisementWatcherBackgroundWorker worker;
+
+        public AdvertisementBackgroundTask()
+        {
+            worker = new AdvertisementWatcherBackgroundWorker();
+            worker.BeaconActionResolved += (sender, action) => { Debug.Write("Action resolved: " + action.PayloadString); };
+        }
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            worker.Run(taskInstance);
+        }
+    }
+}
