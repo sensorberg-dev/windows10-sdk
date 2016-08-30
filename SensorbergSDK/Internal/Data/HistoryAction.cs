2417b73283e6394ed68ff8db99cbcb24811af8d2
kay <eagleeye@byte-welt.net>
2016-08-30 13:13:46 +0200

    code cleanups

--- SensorbergSDK/Internal/Data/HistoryAction.cs
+++ SensorbergSDK/Internal/Data/HistoryAction.cs
@@ -31,7 +31,7 @@ namespace SensorbergSDK.Internal.Data
         {
             if (ReferenceEquals(null, obj)) return false;
             if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
+            if (obj.GetType() != GetType()) return false;
             return Equals((HistoryAction) obj);
         }
 
