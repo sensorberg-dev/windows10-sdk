42c04b20dbb2dd1dc356215a47ccf697acfebe6d
kay <eagleeye@byte-welt.net>
2016-09-09 17:39:46 +0200

    move controls into sdk

---
+++ SensorbergSDK/Controls/BeaconDetailsControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using SensorbergControlLibrary.Model;
+
+namespace SensorbergControlLibrary.Controls
+{
+    public sealed partial class BeaconDetailsControl : UserControl, INotifyPropertyChanged
+    {
+        public static readonly DependencyProperty BeaconDetailProperty = DependencyProperty.Register("BeaconDetail", typeof(BeaconDetailsItem), typeof(BeaconDetailsControl), new PropertyMetadata(default(BeaconDetailsItem)));
+
+        public BeaconDetailsControl()
+        {
+            InitializeComponent();
+        }
+
+        public BeaconDetailsItem BeaconDetail
+        {
+            get { return (BeaconDetailsItem) GetValue(BeaconDetailProperty); }
+            set
+            {
+                SetValue(BeaconDetailProperty, value); 
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
